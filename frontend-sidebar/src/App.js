2c75f94fa3d92226c840b71f5ac5b46653d237ef
rosied <duanrd@student.unimelb.edu.au>
2021-05-05 13:28:34 +1000

    upload the draft of sidebar

---
+++ frontend-sidebar/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React from "react";
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import {BarChart,WordCloud} from './pages/diagram';
+import {Language,Sentiment} from "./pages/map";
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/pages/home' exact component={Home} />
+          <Route path='/map/language' exact component={Language} />
+          <Route path='/map/sentiment' exact component={Sentiment} />
+          <Route path='/diagram/barchart' exact component={BarChart} />
+          <Route path='/diagrams/wordcloud' exact component={WordCloud} />
+        </Switch>
+      </Router>
+      </div>
+    )
+  }
+}
+export default App;
+
---
+++ frontend-sidebar/src/pages/map.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+export const Language = () => {
+  return (
+    <div>
+      <h1>Language Map</h1>
+    </div>
+  );
+};
+export const Sentiment = () => {
+  return (
+    <div>
+      <h1>Sentiment analysis</h1>
+    </div>
+  );
+};
