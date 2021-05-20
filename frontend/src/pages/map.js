651a6f2e75fc25641f957a123b65d862e9e8cdda
rosied <duanrd@student.unimelb.edu.au>
2021-05-20 21:54:30 +1000

    add front and backend link, change bar chart format

--- frontend/src/pages/map.js
+++ frontend/src/pages/map.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState,useEffect} from 'react';
 import LanguageMap from "../components/languageMap";
 import TwitterCountMap from "../components/twitterCountMap";
 // Todo: Retrieve from backend
 import languageData from "../data/sampleData/sampleLanguage.json"
 import twitterCountData from "../data/sampleData/sampleTwitterCount.json"
-
+import axios from 'axios';
 export const Language = () => {
+  const [result, setResult] = useState(null);
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>{
+      const data=response.data;
+      console.log("The data is ");
+      console.log(data);
+      setResult(data[2].company.name);
+      // axios returns API response body in .data
+    })
+  }, []);
+  console.log("The result gotten from language data");
+  console.log(result);
   return (
     <div className="page">
       <h1>Language Map</h1>
@@ -15,6 +28,19 @@ export const Language = () => {
 };
 
 export const TwitterCount = () => {
+  const [result, setResult] = useState(null);
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>{
+      const data=response.data;
+      console.log("The data is ");
+      console.log(data);
+      setResult(data[2].company.name);
+      // axios returns API response body in .data
+    })
+  }, []);
+  console.log("The result gotten from Twitter count");
+  console.log(result);
   return (
     <div className="page">
       <h1>Twitter Count</h1>
